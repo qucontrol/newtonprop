8421882df71acd4146a80ed6bbc592e334b13561
Michael Goerz <goerz@stanford.edu>
2018-12-08 12:49:44 -0500

    Extend documentation of NewtonPropagatorBase

--- src/newtonprop/interface.py
+++ src/newtonprop/interface.py
@@ -7,7 +7,7 @@ stateful propagator that only does the conversion once, holds the required
 state, and converts back only on demand.
 
 While a stateful propagator could be achieved through a wrapper function and
-closures, a more straightfoward implementation holds the necessary state in a
+closures, a more straightforward implementation holds the necessary state in a
 class. To encourage a consistent interface, you should subclass
 :class:`NewtonPropagatorBase` for this purpose.
 """
@@ -22,9 +22,10 @@ __all__ = ["NewtonPropagatorBase"]
 
 
 class NewtonPropagatorBase(metaclass=ABCMeta):
-    """Stateful interface for using the Newton propagator
+    """Abstract base class for a stateful interface for using the Newton
+    propagator
 
-    This must be subclassed before it can be used.
+    This must be sub-classed before it can be used.
 
     Args:
         data: data that underlies the abstract operator $A$. The propagator
@@ -32,17 +33,71 @@ class NewtonPropagatorBase(metaclass=ABCMeta):
             argument `A` for :func:`newtonprop.propagator.step`.
         func (callable or str): The scalar version of the operator-function
             $f$.  Must take one complex argument and return a
-            complex value. If given as a string, the corresponding option will
-            be looked up in :attr:`funcs`.
-        m_max (int): Maximal Krylov dimension
-        maxrestart (int): Maximum number of Newton restarts
-        tol (float): Desired precision of the result
+            complex value. If given as a string, the corresponding callable
+            will be looked up in :attr:`funcs`.
+        m_max (int): Maximal Krylov dimension. Initializes the corresponding
+            attribute.
+        maxrestart (int): Maximum number of Newton restarts. Initializes the
+            corresponding attribute.
+        tol (float): Desired precision of the result. Initializes the
+            corresponding attribute.
 
-    Subclasses will customize the behavior mostly by overriding private
-    methods. See the source code of :class:`NewtonPropagatorBase` for details.
+    Subclasses will customize the behavior mostly by overriding a number of
+    private methods, outlined below. These methods may act on the following
+    (private) attributes:
+
+    .. py:attribute:: _data
+
+        Internal data that may be used to calculate $A$. Obtained from
+        `data` via :meth:`_convert_data_to_internal`.
+
+    .. py:attribute:: _v0
+
+        The state set by :meth:`set_initial_state`, in its original format
+
+    .. py:attribute:: _v
+
+        The current state, in an internal format. The internal format is
+        determined by :meth:`_convert_state_to_internal`.
+
+    **Private methods:**
+
+    .. automethod:: _check
+
+    .. automethod:: _convert_state_to_internal
+
+    .. automethod:: _convert_state_from_internal
+
+    .. automethod:: _convert_data_to_internal
+
+    .. automethod:: _inner
+
+    .. automethod:: _norm
+
+    .. automethod:: _zero
+
+    .. automethod:: _A
+
+    **Public methods, attributes, and properties:**
+
+    .. py:attribute:: func
+
+        The scalar version of the operator-function $f$, as a callable
+
+    .. py:attribute:: m_max
+
+        Maximal Krylov dimension (int)
+
+    .. py:attribute:: maxrestart
+
+        Maximum number of Newton restarts (int)
+
+    .. py:attribute:: tol
+
+        Desired precision of the result (float)
     """
 
-    #: Registry of `func` names
+    #: Registry of `func` names (class attribute)
     #:
     #: Cf. the `func` argument of :func:`newtonprop.propagator.step`. You may
     #: extend this with your own function names.
@@ -70,7 +125,7 @@ class NewtonPropagatorBase(metaclass=ABCMeta):
 
     @property
     def state(self):
-        """The current state
+        """The current state (read-only)
 
         After a call to :meth:`set_initial_state`, this is the state set by
         that call. After a call to :meth:`step`, it is the result of that
@@ -138,37 +193,106 @@ class NewtonPropagatorBase(metaclass=ABCMeta):
             pass
 
     def _convert_state_to_internal(self, v):  # pragma: nocover
-        # This method may convert the initial state v into a more efficient
-        # internal format
+        """Convert the initial state `v` into a more efficient internal format.
+
+        This is used when setting :attr:`_v` from the `v` given in
+        :meth:`set_initial_state`. By default, the state is passed on
+        unchanged.
+        """
         return v
 
     def _convert_state_from_internal(self, v):  # pragma: nocover
-        # If `self._v` uses an internal format different from the `v` in
-        # `set_initial_state`, this method most be overridden to convert
-        # `self._v` back to the original format.
+        """Convert `v` in internal format of :attr:`_v` to original format
+
+        If :attr:`_v` uses an internal format different from the `v` in
+        :meth:`set_initial_state`, this method must be overridden to convert
+        :attr:`_v` back to the original format. It is used by :attr:`state` for
+        this purpose. By default, the internal state is passed on unchanged.
+        """
         return v
 
     @staticmethod
     def _convert_data_to_internal(data):  # pragma: nocover
+        """Convert `data` to :attr:`_data`
+
+        Optional conversion of the `data` passed to the initializer to a more
+        efficient internal format.  By default, the `data` is stored unchanged.
+        """
         return data
 
     @abstractmethod
     def _inner(self, a, b):  # pragma: nocover
-        # Inner product between two states in the format of `self._v`
+        """Inner product between two states `a`, `b` in the format of
+        :attr:`_v`
+
+        Must return a complex number. This is an abstract method, and thus must
+        be implemented by any subclass of :class:`NewtonPropagatorBase`.
+
+        An example implementation is::
+
+            def _inner(self, a, b):
+                return numpy.vdot(a, b)
+        """
         pass
 
     @abstractmethod
     def _norm(self, a):  # pragma: nocover
-        # Norm of a state in the format of `self._v`
+        """Norm of a state `a` in the format of :attr:`_v`
+
+        Must return a complex number. This is an abstract method, and thus must
+        be implemented by any subclass of :class:`NewtonPropagatorBase`.
+
+        An example implementation is::
+
+            def _norm(self, a):
+                return numpy.linalg.norm(a)
+        """
         pass
 
     @abstractmethod
     def _zero(self, a):  # pragma: nocover
-        # A new-zero state compatible with the format of `self._v`
+        """A new zero-state compatible with the format of :attr:`_v`
+
+        Must allocate and return a new state in the same internal format as
+        :attr:`_v`, but containing zeros, so that the state can set in-place.
+        This is an abstract method, and thus must be implemented by any
+        subclass of :class:`NewtonPropagatorBase`.
+
+        An example implementation is::
+
+            def _zero(self, a):
+                return numpy.zeros(shape=a.shape, dtype=a.dtype)
+        """
         pass
 
     @abstractmethod
     def _A(self, t):  # pragma: nocover
-        # return a callable `A(v)` that takes a state `v` in the
-        # format of `self._v` and returns a state in the same format
+        """Callable ``A(v)`` encoding the abstract operator $A(t)$.
+
+        Must return a callable ``A(v)`` that takes a state `v` in the
+        internal format of :attr:`_v` and returns a state in the same format.
+        This is an abstract method, and thus must be implemented by any
+        subclass of :class:`NewtonPropagatorBase`.
+
+        Assuming ``self._data`` contains a nested list ``[L0, [L1, eps]]``
+        where ``L0`` and ``L1`` are numpy matrices containing a superoperator,
+        and ``eps`` is a callable with argument `t` for a time-dependent
+        control, an example implementation is::
+
+            def _A(self, t):
+                Lmatrix = (
+                    self._data[0] + self._data[1][0] + self._data[1][1](t))
+
+                def L(v):
+                    return Lmatrix @ v
+
+                return L
+
+        .. Note::
+
+            This method does not calculate ``A(v)`` directly, but returns a
+            callable that does. This is to take into account time-dependent
+            operators, which :func:`newtonprop.propagator.step` does not know
+            anything about.
+        """
         pass
